fa69716de02221d39fac6492466916565204f2f7
Joshua Vial <joshua@enspiral.com>
2024-07-06 10:14:11 +1200

    slides theme

---
+++ holochain-theme/layouts/end.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="slidev-layout cover relative">
+    <div class="absolute -z-50 w-full h-full translate-x-[36%]">
+      <img
+        src="/assets/big-circle.svg"
+        alt=""
+        class="h-[100%] animate-spin-slow float-right"
+      />
+    </div>
+    <div
+      class="w-[60%] text-center mx-auto flex items-center flex-col justify-center font font-serif"
+    >
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.animate-spin-slow {
+  animation: spin 500s linear infinite;
+}
+
+@keyframes spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+</style>
---
+++ holochain-theme/layouts/intro.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="slidev-layout cover relative">
+    <div class="absolute -z-50">
+      <img
+        src="/assets/title.png"
+        alt=""
+        class="translate-x-[-5%] -scale-y-101"
+      />
+    </div>
+    <div class="flex items-center justify-end font font-serif">
+      <div class="flex w-[62%] flex-col">
+        <slot />
+      </div>
+    </div>
+  </div>
+</template>
