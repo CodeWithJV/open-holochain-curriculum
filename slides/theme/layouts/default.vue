bf5710abae22f65fba58b6837e273f155b83d5c9
Joshua Vial <joshua@enspiral.com>
2024-07-06 21:15:30 +1200

    Added the correct path to the big-circle.svg image.

--- slides/theme/layouts/default.vue
+++ slides/theme/layouts/default.vue
@@ -4,7 +4,7 @@
       class="absolute -z-50 w-full h-full translate-x-[40%] translate-y-[69%]"
     >
       <img
-        src="/assets/big-circle.svg"
+        src="./../assets/big-circle.svg"
         alt=""
         class="h-[150%] float-right rotate-[30deg]"
       />
